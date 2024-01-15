e0c99cd6e5365949af2f7edf47629145974c99d1
Donghyuk Lee <leedonghyuk2163@gmail.com>
2024-01-15 10:49:25 +0900

    Feat: 5분마다 데이터 패치, 오늘 날짜 새로고침

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -10,99 +10,102 @@ dayjs.locale("ko");
 
 const Schedule = () => {
   const dummyData = {
-    timestamp: "2024-01-12T16:49:17",
-    data: [
-      {
-        meetingDt: "2024-01-02",
-        roomNm: "2층 회의실",
-        startTm: "11:00",
-        endTm: "12:00",
-        title: "사업기획 회의",
-        registrant: "유지원",
-        regDt: "2024-01-02T14:09",
-        department: "R&D팀",
-      },
-      {
-        meetingDt: "2024-01-02",
-        roomNm: "2층 회의실",
-        startTm: "14:30",
-        endTm: "15:30",
-        title: "본부장님-팀장 회의",
-        registrant: "유지원",
-        regDt: "2024-01-02T14:09",
-        department: "R&D팀",
-      },
-      {
-        meetingDt: "2024-01-02",
-        roomNm: "3층 회의실",
-        startTm: "16:00",
-        endTm: "16:30",
-        title: "테스트",
-        registrant: "이용훈",
-        regDt: "2024-01-02T14:36",
-        department: "(주)드제이",
-      },
-      {
-        meetingDt: "2024-01-03",
-        roomNm: "3층 회의실",
-        startTm: "21:00",
-        endTm: "22:00",
-        title: "테스트 일정",
-        registrant: "이동혁",
-        regDt: "2024-01-03T16:46",
-        department: "개발2팀",
-      },
-      {
-        meetingDt: "2024-01-03",
-        roomNm: "3층 회의실",
-        startTm: "19:00",
-        endTm: "20:00",
-        title: "test2",
-        registrant: "이동혁",
-        regDt: "2024-01-03T19:28",
-        department: "개발2팀",
-      },
-      {
-        meetingDt: "2024-01-04",
-        roomNm: "2층 회의실",
-        startTm: "10:00",
-        endTm: "10:30",
-        title: "어플드제이 일일보고 참석",
-        registrant: "유지원",
-        regDt: "2024-01-04T18:00",
-        department: "R&D팀",
-      },
-      {
-        meetingDt: "2024-01-04",
-        roomNm: "3층 회의실",
-        startTm: "10:00",
-        endTm: "12:00",
-        title: "회의실 예약 테스트",
-        registrant: "이동혁",
-        regDt: "2024-01-04T12:26",
-        department: "개발2팀",
-      },
-      {
-        meetingDt: "2024-01-05",
-        roomNm: "2층 회의실",
-        startTm: "08:00",
-        endTm: "18:00",
-        title: "TEST 회의실 예약",
-        registrant: "이동혁",
-        regDt: "2024-01-05T17:45",
-        department: "개발2팀",
-      },
-      {
-        meetingDt: "2024-01-11",
-        roomNm: "2층 회의실",
-        startTm: "14:00",
-        endTm: "17:00",
-        title: "2024 중소벤처기업 지원사업 종합설명회(온라인) 참석",
-        registrant: "유지원",
-        regDt: "2024-01-10T14:34",
-        department: "R&D팀",
-      },
-    ],
+    msg: "성공",
+    data: {
+      timestamp: "2024-01-14T00:17:49",
+      data: [
+        {
+          meetingDt: "2024-01-02",
+          roomNm: "2층 회의실",
+          startTm: "11:00",
+          endTm: "12:00",
+          title: "사업기획 회의",
+          registrant: "유지원",
+          regDt: "2024-01-02T14:09:00",
+          department: "R&D팀",
+        },
+        {
+          meetingDt: "2024-01-02",
+          roomNm: "2층 회의실",
+          startTm: "14:30",
+          endTm: "15:30",
+          title: "본부장님-팀장 회의",
+          registrant: "유지원",
+          regDt: "2024-01-02T14:09:00",
+          department: "R&D팀",
+        },
+        {
+          meetingDt: "2024-01-02",
+          roomNm: "3층 회의실",
+          startTm: "16:00",
+          endTm: "16:30",
+          title: "테스트",
+          registrant: "이용훈",
+          regDt: "2024-01-02T14:36:00",
+          department: "(주)드제이",
+        },
+        {
+          meetingDt: "2024-01-03",
+          roomNm: "3층 회의실",
+          startTm: "21:00",
+          endTm: "22:00",
+          title: "테스트 일정",
+          registrant: "이동혁",
+          regDt: "2024-01-03T16:46:00",
+          department: "개발2팀",
+        },
+        {
+          meetingDt: "2024-01-03",
+          roomNm: "3층 회의실",
+          startTm: "19:00",
+          endTm: "20:00",
+          title: "test2",
+          registrant: "이동혁",
+          regDt: "2024-01-03T19:28:00",
+          department: "개발2팀",
+        },
+        {
+          meetingDt: "2024-01-04",
+          roomNm: "2층 회의실",
+          startTm: "10:00",
+          endTm: "10:30",
+          title: "어플드제이 일일보고 참석",
+          registrant: "유지원",
+          regDt: "2024-01-04T18:00:00",
+          department: "R&D팀",
+        },
+        {
+          meetingDt: "2024-01-04",
+          roomNm: "3층 회의실",
+          startTm: "10:00",
+          endTm: "12:00",
+          title: "회의실 예약 테스트",
+          registrant: "이동혁",
+          regDt: "2024-01-04T12:26:00",
+          department: "개발2팀",
+        },
+        {
+          meetingDt: "2024-01-05",
+          roomNm: "2층 회의실",
+          startTm: "08:00",
+          endTm: "18:00",
+          title: "TEST 회의실 예약",
+          registrant: "이동혁",
+          regDt: "2024-01-05T17:45:00",
+          department: "개발2팀",
+        },
+        {
+          meetingDt: "2024-01-11",
+          roomNm: "2층 회의실",
+          startTm: "14:00",
+          endTm: "17:00",
+          title: "2024 중소벤처기업 지원사업 종합설명회(온라인) 참석",
+          registrant: "유지원",
+          regDt: "2024-01-10T14:34:00",
+          department: "R&D팀",
+        },
+      ],
+    },
   };
   const roomType = ["2층 회의실", "3층 회의실"];
 
@@ -111,25 +114,29 @@ const Schedule = () => {
   const [currentDate, setCurrentDate] = useState(dayjs());
   const [roomState, setRoomState] = useState("회의실 체크인");
 
-  // useEffect(() => {
-  //   fetch("/meetingRoom/get", {
-  //     method: "GET",
-  //   })
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setData(data);
-  //     });
-  // }, []);
+  useEffect(() => {
+    setInterval(() => {
+      fetch("/meetingRoom/get", {
+        method: "GET",
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+        });
 
-  // console.log("🚀data", data);
+      setCurrentDate(dayjs());
+    }, 60000);
+  }, []);
+
+  console.log("🚀data", data);
 
   const handleRoomChange = (newRoom) => {
     setRoomName(newRoom);
   };
 
-  const filteredData = dummyData.data.filter((item) => {
+  const filteredData = data?.data.data.filter((item) => {
     return (
       dayjs(item.meetingDt).format("YYYY-MM-DD") ===
         currentDate.format("YYYY-MM-DD") &&
