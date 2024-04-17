6d2c46b057bd24abdd4bf35e55bb0f8240d2086d
Donghyuk Lee <leedonghyuk2163@gmail.com>
2024-04-17 11:32:41 +0900

    Revert "Fix: host 주소 변경"
    This reverts commit 5d49a92c51ca2ee86896035c026b803aa1a79591.

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -44,7 +44,7 @@ const Schedule = () => {
   };
 
   const fetchData = () => {
-    fetch("host.docker.internal:8090/meetingRoom/get", {
+    fetch("http://localhost:8090/meetingRoom/get", {
       method: "GET",
     })
       .then((response) => {
